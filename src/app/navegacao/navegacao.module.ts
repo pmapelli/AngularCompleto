f481f599eee9d8320285e5968886f77cd6674cc5
pedromapelli <pedro.m@lyncas.net>
2022-03-15 08:35:53 -0300

    feat: Configurando o pacote de bootstrap

--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from "./home/home.component";
 import { MenuComponent } from "./menu/menu.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { MenuLoginComponent } from "./menu-login/menu-login.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
     declarations: [
@@ -17,7 +18,8 @@ import { MenuLoginComponent } from "./menu-login/menu-login.component";
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        NgbModule
     ],
     exports: [
         MenuComponent,
